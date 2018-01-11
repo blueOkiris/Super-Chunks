b026aa2e1fc39365492ac5fc8269dc87d9319768
blueOkiris <dylantdmt@gmail.com>
2018-01-11 14:19:28 -0500

    msg

---
+++ update_git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Save all changes
+git add *
+
+# Get commit message and commit changes
+echo "Enter commit message: "
+read msg
+git commit -m msg
+
+# Update github
+git push -f origin master
+
