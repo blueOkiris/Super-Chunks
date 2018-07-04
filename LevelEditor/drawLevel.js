a7d6eb5857446ea860c9f787259ea9aabce79140
Dylan Turner <dylantdmt@gmail.com>
2018-07-03 21:56:16 -0400

    Started work on Level Editor for ease-of-use

---
+++ LevelEditor/drawLevel.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("levelCanvas");
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "#FFFFFF";
+ctx.fillRect(0, 0, 800, 600);
+
+function drawEditLevel() {
+	
+}
+
+document.body.onload = drawEditLevel();
