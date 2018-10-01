71978f9f9d419151b6d053807c8ed9685e626627
Dylan Turner <dylantdmt@gmail.com>
2018-09-30 22:20:32 -0400

    Basic Window for new Editor

--- Level_Editor_2/main.c
+++ Level_Editor_2/main.c
@@ -1,14 +1,29 @@
-#include <gtk/gtk.h>
+#include <gtk-3.0/gtk/gtk.h>
 
 #define DEFAULT_SCREEN_WIDTH    800
 #define DEFAULT_SCREEN_HEIGHT   600
 
+static void print_hello(GtkWidget* widget, gpointer data) {
+    g_print("Hello, world!\n");
+}
+
 static void activate(GtkApplication* app, gpointer user_data) {
     GtkWidget* window;
+    GtkWidget* button;
+    GtkWidget *button_box;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title (GTK_WINDOW(window), "Super-Chunks Level Editor");
     gtk_window_set_default_size(GTK_WINDOW(window), DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
+    
+    button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_container_add(GTK_CONTAINER(window), button_box);
+
+    button = gtk_button_new_with_label("Hello, world!");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+    gtk_container_add(GTK_CONTAINER(button_box), button);
+
     gtk_widget_show_all(window);
 }
 
