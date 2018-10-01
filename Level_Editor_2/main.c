e8f068cb592487d59808db47a823f09c613e553c
Dylan Turner <dylantdmt@gmail.com>
2018-09-30 23:03:50 -0400

    Basic structure for new Lev Editor

--- Level_Editor_2/main.c
+++ Level_Editor_2/main.c
@@ -7,35 +7,37 @@ static void print_hello(GtkWidget* widget, gpointer data) {
     g_print("Hello, world!\n");
 }
 
-static void activate(GtkApplication* app, gpointer user_data) {
-    GtkWidget* window;
-    GtkWidget* button;
-    GtkWidget *button_box;
-
-    window = gtk_application_window_new(app);
-    gtk_window_set_title (GTK_WINDOW(window), "Super-Chunks Level Editor");
-    gtk_window_set_default_size(GTK_WINDOW(window), DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
-    
-    button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-    gtk_container_add(GTK_CONTAINER(window), button_box);
-
-    button = gtk_button_new_with_label("Hello, world!");
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-    gtk_container_add(GTK_CONTAINER(button_box), button);
-
-    gtk_widget_show_all(window);
-}
-
 int main(int argc, char** argv) {
-    GtkApplication* app;
-    int status;
+    GtkBuilder* builder;
+    GObject* window;
+    GObject* button;
+    GError* error = NULL;
 
-    app = gtk_application_new("io.github.blueokiris", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    gtk_init(&argc, &argv);
 
-    status = g_application_run(G_APPLICATION(app), argc, argv);
+    /* Construct a GtkBuilder using our UI description */
+    builder = gtk_builder_new();
+    if(gtk_builder_add_from_file(builder, "builder.xml", &error) == 0) {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
 
-    g_object_unref(app);
-    return status;
+        return 1;
+    }
+
+    /* Connect signal handlers to the constructed widgets. */
+    window = gtk_builder_get_object(builder, "window");
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    button = gtk_builder_get_object(builder, "button1");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+    button = gtk_builder_get_object(builder, "button2");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+    button = gtk_builder_get_object(builder, "quit");
+    g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_main();
+
+    return 0;
 }
